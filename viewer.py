a36b0f5bf2d32ad048b3f18cec0f0dccadbdaf28
Matthieu Heitz <heitz.matthieu@gmail.com>
2021-05-13 19:33:45 -0700

    Comment debugging helpers

--- viewer.py
+++ viewer.py
@@ -129,12 +129,11 @@ class Ui(QtWidgets.QMainWindow):
         # self.print(str(self.graphicsView.viewport().size()))    # Prints the size of the widget, not the scene bounding box coordinates
 
         # DEBUG
-        self.lineEdit_mainFolder.setText("/home/matthieu/Work/Postdoc-UBC/Projects/trajectory_inference/DGCG_scRNAseq/examples/results_reprog_umap_2d_ss10__sig_ab_multistart")
-        self.mainFolder_changed()
-        time.sleep(0.5)
-        self.lineEdit_filePattern.setText("iter_001_insertion.png")
-        self.filePattern_changed()
-        # self.lineEdit_filePattern.setText("iter_*_insertion.png[-1]")
+        # self.lineEdit_mainFolder.setText("")
+        # self.mainFolder_changed()
+        # time.sleep(0.5)
+        # self.lineEdit_filePattern.setText("")
+        # self.filePattern_changed()
 
 
     def resizeEvent(self, event):   # This is an overloaded function
