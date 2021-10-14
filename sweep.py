87ee1784409df6cca1c31350bc29b530954424e3
Matthieu Heitz <heitz.matthieu@gmail.com>
2021-10-14 14:14:44 -0700

    Don't keep the CSV file opened during the entire sweep

--- sweep.py
+++ sweep.py
@@ -15,18 +15,12 @@ import csv
 #                    - if `sweep_dir` is not empty, a path to the experiment directory
 # - sweep_dir: the main directory in which to store all the experiment directories
 # - start_index: optional argument that sets the first experiment ID.
-# - result_csv_filename: a optional CSV filename to write individual results of each experiment. Each experiment will
+# - result_csv_filename: an optional CSV filename to write individual results of each experiment. Each experiment will
 #                        have one row in that CSV. If this is set, then experiment_func must return the results as a
 #                        dictionary, with keys being the column names, and values the value of each result for that
-#                        experiment.
+#                        experiment. The results are written individually to the file as soon as they are obtained,
+#                        so that the file is readable during the sweep.
 def parameter_sweep(param_dict, experiment_func, sweep_dir, start_index=0, result_csv_filename=""):
-
-    if result_csv_filename:
-        # Create the csv file
-        csv_file = open(os.path.join(sweep_dir, result_csv_filename), mode='w')
-        csv_writer = csv.writer(csv_file)
-    else:
-        csv_writer = None
 
     # Fill the current_dict and count number of experiments
     current_dict={}
@@ -34,6 +28,8 @@ def parameter_sweep(param_dict, experiment_func, sweep_dir, start_index=0, resul
     for k in param_dict.keys():
         current_dict[k] = param_dict[k][0]
         num_exp *= len(param_dict[k])
+
+    print("\nThere are a total of",num_exp,"experiments.\n")
 
     def recursive_call(exp_id, current_dict, param_index):
         current_key = list(param_dict.keys())[param_index]
@@ -55,12 +51,17 @@ def parameter_sweep(param_dict, experiment_func, sweep_dir, start_index=0, resul
                     if result_dict:
                         # On first experiment, write the CSV header
                         if exp_id == start_index:
-                            csv_writer.writerow(["exp_id"] + list(param_dict.keys()) + list(result_dict.keys()))
+                            # Create the csv file, write the header.
+                            with open(os.path.join(sweep_dir, result_csv_filename), mode='w') as csv_file:
+                                csv_writer = csv.writer(csv_file)
+                                csv_writer.writerow(["exp_id"] + list(param_dict.keys()) + list(result_dict.keys()))
 
                         # Save additional results by writing them to the CSV
-                        csv_row = [exp_id] + list(current_dict.values())    # Write exp_id and current param values
-                        csv_row += list(result_dict.values())   # Write returned data
-                        csv_writer.writerow(csv_row)
+                        with open(os.path.join(sweep_dir, result_csv_filename), mode='a') as csv_file:
+                            csv_writer = csv.writer(csv_file)
+                            csv_row = [exp_id] + list(current_dict.values())    # Write exp_id and current param values
+                            csv_row += list(result_dict.values())   # Write returned data
+                            csv_writer.writerow(csv_row)
                     else:
                         print("WARNING: Can't write results to CSV: received 'None' from experiment_func().")
 
@@ -71,6 +72,3 @@ def parameter_sweep(param_dict, experiment_func, sweep_dir, start_index=0, resul
     # Start experiments
     recursive_call(start_index, current_dict, 0)
 
-    # Close CSV file if necessary
-    if result_csv_filename:
-        csv_file.close()
