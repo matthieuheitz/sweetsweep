0df545e929321bcf54e87b53e5dde25deb51c690
Matthieu Heitz <matthieuheitz@users.noreply.github.com>
2022-04-20 00:57:19 -0700

    Change project URL and add comments for building package

--- sweetsweep/__init__.py
+++ sweetsweep/__init__.py
@@ -2,3 +2,8 @@ from .sweep import parameter_sweep, parameter_sweep_parallel
 
 # Define version here
 __version__ = '0.1.1'
+
+
+# Command reminder for building the package
+# python3 -m build
+# python3 -m twine upload --skip-existing dist/*
