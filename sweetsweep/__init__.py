e45d0c2822e1c3e3f183b084bb32083fbc5d05c9
Matthieu Heitz <heitz.matthieu@gmail.com>
2024-07-10 12:23:57 -0700

    New version

--- sweetsweep/__init__.py
+++ sweetsweep/__init__.py
@@ -1,9 +1,10 @@
 from .sweep import parameter_sweep, parameter_sweep_parallel, get_num_exp
 
 # Define version here
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 
 # Command reminder for building the package
+# For upload with twine, username and passord are already stored in ~/.pypirc
 # python3 -m build
 # python3 -m twine upload --skip-existing dist/*
